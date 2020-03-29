ae0d58ac76b0e1c5f75d8878c075ab0feb732f03
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-29 19:02:15 +0200

    przeszło testy

--- url.js
+++ url.js
@@ -1,22 +1,19 @@
 https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 
 function domainName(url) {
-    
-    if (url.indexOf(".") != url.lastIndexOf(".")) {let x=2+url.indexOf("/");}
-    else {let x =2+ url.indexOf(".");}
-    
-    //else { x = url.indexOf("."); };
-    
+    let x = 0;
+    if (url.includes("www")) { x = 1 + url.indexOf(".") }
+    else if (url.includes("http")) { x = 2 + url.indexOf("/"); }
+
     let u = url.slice(x, url.length);
-    
     y = u.indexOf(".");
     let z = u.substring(0, y);
-//*/
-    console.log(x);
-//    console.log(z);
+    //return(z);
+    console.log(z);
 }
 
-//domainName("http://google.com"); //console.log("google");
-//domainName("http://google.co.jp");// console.log("google");
+domainName("http://google.com"); //console.log("google");
+domainName("http://google.co.jp");// console.log("google");
 domainName("www.xakep.ru");// console.log("xakep");
-//domainName("https://youtube.com"); //console.log("youtube");
+domainName("https://youtube.com"); //console.log("youtube");
+domainName("https://developer.mozilla.org/pl/docs");// console.log("developer");
